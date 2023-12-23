cda7837d655a96a6526cc27bdba6ff0bf124d8b7
meetR panchal <panchalmeetr97@gmail.com>
2023-12-23 11:19:56 +0530

    2

--- main.dart
+++ main.dart
@@ -1,8 +1,8 @@
-void main(){    
- int r = 5;  
- double pi = 3.14;  
- double res = 4*pi*r*r;    
- print("The area of sphere = $res"); 
+void main() {
+ int r = 5;
+ double pi = 3.14;
+ double res = 4 * pi * r * r;
+ print("The area of sphere = $res");
  print("meet are BATMAN and also Bruce Wayne ");
- print(r*pi);
-}  
+ print(r * pi);
+}
