9242955f3cc8aeb95b8cc6015086a7ddafe3bc67
MEeTpanchal1222 <143181413+MEeTpanchal1222@users.noreply.github.com>
2024-01-17 17:05:59 +0530

    Add files via upload
    17/1/24

---
+++ 4.1_dart/occupations_clulator.dart
@@ -0,0 +1,13 @@
+ import 'dart:io';
+void main()
+{
+   print(   cul(dsa: 45,c: 56,data_scince: 450)
+);
+
+}
+ double  cul({int? dsa,int? c, int? ct,int? data_scince})
+ {
+    int x =  dsa!+c!+ct!+data_scince!;
+    double y=  x*100/400;
+    return  y;
+ }
